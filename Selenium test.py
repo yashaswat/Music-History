8cc46902242111e3fb76e1e4e68697522648c7d9
yashaswat <86275976+yashaswat@users.noreply.github.com>
2025-01-27 20:57:53 +0530

    extracting album metadata successfully

--- Selenium test.py
+++ Selenium test.py
@@ -6,8 +6,18 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from bs4 import BeautifulSoup
 
+def unique(list):
+    
+    unique_list = []
+    
+    for i in list:
+        if i not in unique_list:
+            unique_list.append(i)
+          
+    return unique_list
+
 url = 'https://www.last.fm/search/albums'
-album = 'Blonde'
+album = 'High Visceral, Pt. 1'
 
 driver = webdriver.Chrome()
 driver.get(url)
@@ -20,12 +30,24 @@ search_bar.send_keys(album)
 search_bar.submit()
 
 '''<a href="/music/Frank+Ocean/Blonde" title="Blonde" class="link-block-target">Blonde</a>'''
-#album = driver.find_element(By.CLASS_NAME, 'link-block-target')
-album = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.LINK_TEXT, album)))
+album = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.LINK_TEXT, album)))
 album.click()
 
 html = driver.page_source
-#driver.save_screenshot('album_page.png')
+driver.save_screenshot('album_page.png')
 #print(driver.current_url)
 
 driver.quit()
+
+soup = BeautifulSoup(html, 'html.parser')
+
+artist = soup.find('span', attrs={'itemprop':'name'}).text
+
+metadata = unique(soup.find_all('dd', attrs={'class':'catalogue-metadata-description'}))
+#print(metadata)
+
+runtime = metadata[0].text
+release = metadata[1].text
+
+album_info = [artist, runtime.strip(), release.strip()]
+print(album_info)
