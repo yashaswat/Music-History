59129c8192bf7cfc110bd7475d7f8e2ae549a9d9
yashaswat <86275976+yashaswat@users.noreply.github.com>
2025-01-31 12:39:18 +0530

    modularized code and removed empty rows

--- note_to_excel.py
+++ note_to_excel.py
@@ -10,62 +10,87 @@ def arrow_index(list):
             return i
     return -1
 
+
+def music_info_to_dict(data, note_path):
+
+    f = open(note_path , 'r')
+
+    for entry in f.readlines():
+        
+        if entry == '\n':
+            continue
+        
+        if ' by ' not in entry:
+            entry = entry.rstrip()
+            data['music project'].append(entry)
+            data['artist'].append('')
+        else:
+            entry = entry.split(' by ')
+            data['music project'].append(entry[0].rstrip())
+            data['artist'].append(entry[1].rstrip())
+    f.close()
+    
+
+def spotify_logging_status(data):
+    
+    for i, album in enumerate(data['music project']):
+
+        if 'â‚¹' not in album:
+            data['spotify logged'].append('Yes')
+            data['music project'][i] = data['music project'][i].replace(' â‚¹', '').lstrip('.')
+        else:
+            data['spotify logged'].append('No')
+            data['music project'][i] = data['music project'][i].replace(' â‚¹', '').lstrip('.')
+   
+    for i, artist in enumerate(data['artist']):
+
+        if 'â‚¹' not in artist:
+            continue
+        else:
+            data['spotify logged'][i] = 'No'
+            data['artist'][i] = data['artist'][i].replace(' â‚¹', '')
+            
+
+def current_progress_marking(data):
+    
+    artist_arrow = arrow_index(data['artist'])
+    album_arrow = arrow_index(data['music project'])
+
+    if album_arrow != -1:
+
+        data['music project'][album_arrow] = data['music project'][album_arrow].replace('<===========', '')
+        data['spotify logged'][album_arrow] = 'Current'
+
+        for i in range(album_arrow+1, len(data['music project'])):
+            data['spotify logged'][i] = 'No'
+        
+    if artist_arrow != -1:
+
+        data['artist'][artist_arrow] = data['artist'][artist_arrow].replace('<===========', '')
+        data['spotify logged'][artist_arrow] = 'Current'
+
+        for i in range(artist_arrow+1, len(data['music project'])):
+            data['spotify logged'][i] = 'No'
+            
+
+note_path = 'album_list.txt'
+
 data = {
     'music project' : [],
     'artist' : [],
+    #'runtime' : [],
+    #'release date' : [],
     'spotify logged' : []
+    #'genre tags' : []
 }
 
-f = open('album_list.txt', 'r')
-
-for i in f.readlines():
-    if ' by ' not in i:
-        i = i.rstrip()
-        data['music project'].append(i)
-        data['artist'].append('')
-    else:
-        i = i.split(' by ')
-        data['music project'].append(i[0].rstrip())
-        data['artist'].append(i[1].rstrip())
-f.close()
-
-for i, album in enumerate(data['music project']):
-
-    if 'â‚¹' not in album:
-        data['spotify logged'].append('Yes')
-        data['music project'][i] = data['music project'][i].replace(' â‚¹', '').lstrip('.')
-    else:
-        data['spotify logged'].append('No')
-        data['music project'][i] = data['music project'][i].replace(' â‚¹', '').lstrip('.')
-   
-for i, artist in enumerate(data['artist']):
-        
-    if 'â‚¹' not in artist:
-        continue
-    else:
-        data['spotify logged'][i] = 'No'
-        data['artist'][i] = data['artist'][i].replace(' â‚¹', '')
-
-artist_arrow = arrow_index(data['artist'])
-album_arrow = arrow_index(data['music project'])
-
-if album_arrow != -1:
-    data['music project'][album_arrow] = data['music project'][album_arrow].replace('<===========', '')
-    data['spotify logged'][album_arrow] = 'Current'
-    
-    for i in range(album_arrow+1, len(data['music project'])):
-        data['spotify logged'][i] = 'No'
-        
-if artist_arrow != -1:
-    data['artist'][artist_arrow] = data['artist'][artist_arrow].replace('<===========', '')
-    data['spotify logged'][artist_arrow] = 'Current'
-    
-    for i in range(artist_arrow+1, len(data['music project'])):
-        data['spotify logged'][i] = 'No'
+music_info_to_dict(data, note_path)
+spotify_logging_status(data)
+current_progress_marking(data)
 
 df = pd.DataFrame(data)
 
-datatoexcel = pd.ExcelWriter('music_historyX.xlsx')
+datatoexcel = pd.ExcelWriter('my_music_history.xlsx')
 df.to_excel(datatoexcel)
 datatoexcel.close()
 
---
+++ test.py
@@ -0,0 +1,7 @@
+f = open('album_list.txt', 'r')
+
+for i  in f.readlines()[15:21]:
+    list = []
+    list.append(i)
+    
+    print(list)
