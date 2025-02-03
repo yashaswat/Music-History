ba85c4a3e8d93ef10fdc371d428d96fcf7c49628
yashaswat <86275976+yashaswat@users.noreply.github.com>
2025-02-03 23:05:09 +0530

    testing to resolve timeout errors

--- test.py
+++ test.py
@@ -1,9 +1,58 @@
-f = open('album_list.txt', 'r')
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-for i  in f.readlines()[15:21]:
-    list = []
-    list.append(i)
+
+def webdriver_init():
+        
+    options = webdriver.ChromeOptions()
+    # options.add_argument('--headless=new')
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-features=UseChromeML")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    # options.add_argument("disable-accelerated-2d-canvas")
+    # options.add_argument("--disable-software-rasterizer")  
+    # options.add_argument("--disable-dev-shm-usage")
+      
+    # options.add_experimental_option('prefs', {'profile.managed_default_content_settings.images': 2,
+    #                                       'profile.managed_default_content_settings.javascript': 2})
+
+    driver = webdriver.Chrome(options=options)
     
-    print(list)
+    driver.get('https://www.last.fm/search/albums')
     
-lst = []
+    return driver
+
+def hello(driver, album):
+    
+    search_bar = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'site-search')))
+    search_bar.click()
+    
+    search_bar.send_keys(album)
+    search_bar.submit()
+    
+    right_tab = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="mantle_skin"]/div[2]/div/div[3]/nav/ul/li[3]/a')))
+    right_tab.click()
+    
+    album = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, album)))
+    album.click()
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'header-new-title')))
+    html = driver.page_source
+    driver.save_screenshot('album_page.png')
+    
+    reset = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, 'Last.fm')))
+    reset.click()
+    return_to_search = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'masthead-search-toggle')))
+    return_to_search.click()
+    
+    return html
+
+album = ['Insano', 'Blonde', 'Continuum', 'Room for Squares', 'Heavier Things']
+
+hi = webdriver_init()
+
+for i in album:
+    hello(hi, i)
