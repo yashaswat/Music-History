348fbcc0571d60bc2c17369f0ae14b3730dc64ff
yashaswat <86275976+yashaswat@users.noreply.github.com>
2025-02-27 03:05:57 +0530

    comments for music_scrape module

--- music_scrape.py
+++ music_scrape.py
@@ -11,6 +11,7 @@ from bs4 import BeautifulSoup
 import undetected_chromedriver as uc
 
 
+# loading a random user agent for avoiding bot detection
 def load_user_agent():
     
     user_agents = [
@@ -22,6 +23,7 @@ def load_user_agent():
     return random.choice(user_agents)
 
 
+# set-up webdriver options
 def load_options(random_agent):
     
     options = uc.ChromeOptions()
@@ -37,12 +39,14 @@ def load_options(random_agent):
     
     options.add_argument(f'user-agent={random_agent}')
     
+    # block javascript and images
     options.add_experimental_option('prefs', {'profile.managed_default_content_settings.images': 2,
                                           'profile.managed_default_content_settings.javascript': 2})
     
     return options
 
 
+# initialize webdriver with agent and options
 def webdriver_init(url='https://www.last.fm/search/albums'):
 
     random_agent = load_user_agent()
@@ -54,43 +58,44 @@ def webdriver_init(url='https://www.last.fm/search/albums'):
     
     return driver
 
-
+# go to album page on last.fm, get html code and reset
 def target_html(driver, album):
 
     wait = WebDriverWait(driver, 5)
     
-    # <input id="site-search" class="search-field" type="text" name="q" placeholder="Search for music…" value="" required="">
+    # click on the search bar
     search_bar = wait.until(EC.presence_of_element_located((By.ID, 'site-search')))
     search_bar.click()
     
-    # time.sleep(1)
-
+    # clear existing text and enter album name
     search_bar.clear()
     search_bar.send_keys(album)
     search_bar.submit()
     
+    # go to the "album" tab on search
     album_tab = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="mantle_skin"]/div[2]/div/div[3]/nav/ul/li[3]/a')))
     album_tab.click()
     
     time.sleep(1)
     
-    # <a href="/music/Frank+Ocean/Blonde" title="Blonde" class="link-block-target">Blonde</a>
+    # click album
     album = wait.until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, album)))
     album.click()
     
-    # time.sleep(1)
-    
+    # wait for page load and fetch html
     wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'header-new-title')))
     html = driver.page_source
     driver.save_screenshot('Screenshots/album_page.png')
     
     time.sleep(1)
     
+    # return to site home page
     homepage_return = wait.until(EC.presence_of_element_located((By.LINK_TEXT, 'Last.fm')))
     homepage_return.click()
     
     wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'spike_intro')))
     
+    # reset to search bar
     back_to_searchbar = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'masthead-search-toggle')))
     back_to_searchbar.click()
     
@@ -100,21 +105,21 @@ def target_html(driver, album):
     
     return html
 
-
+# extract relevant info about album from html
 def fetch_album_info(driver, album):
 
     album_page_html = target_html(driver, album)
     soup = BeautifulSoup(album_page_html, 'html.parser')
 
-    artist = soup.find('span', attrs={'itemprop':'name'}).text
+    artist = soup.find('span', attrs={'itemprop':'name'}).text # artist name
     
-    tags_list = soup.find_all('li', attrs={'class':'tag'})
+    tags_list = soup.find_all('li', attrs={'class':'tag'}) # all genre tags
     genre_tags = []
     
     for tag in tags_list[:5]:
         genre_tags.append(tag.text)
         
-    metadata = soup.find_all('dd', attrs={'class':'catalogue-metadata-description'})
+    metadata = soup.find_all('dd', attrs={'class':'catalogue-metadata-description'}) # runtime and release date
 
     try:
         if metadata:
@@ -133,6 +138,8 @@ def fetch_album_info(driver, album):
 
 
 if __name__ == '__main__':
+    
+    # testing code
     
     test_album = str(input('Enter Album to search: '))
     driver = webdriver_init()
