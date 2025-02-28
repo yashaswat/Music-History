513b3b3b433603835bd0e9dd42eff371f2a05841
yashaswat <86275976+yashaswat@users.noreply.github.com>
2025-02-28 14:52:12 +0530

    functions to update data to and from gkeep
    update functions work on local txt file for data safety and backup

--- gkeep.py
+++ gkeep.py
@@ -9,21 +9,63 @@ master_token = auth.master_token
 note_id = auth.note_id # album list note
 
 
-def gkeep_get(email, master_token, note_id):
+# connect to google keep account
+def connect(mail=email, token=master_token):
     
-    keep = gkeepapi.Keep()
-    keep.authenticate(email, master_token) # establish connection
+    glink = gkeepapi.Keep()
+    glink.authenticate(mail, token) # establish connection
+    return glink
+
+
+# get data from gkeep note    
+def note_data(note_id=note_id):
+    
+    keep = connect()
 
     gnote = keep.get(note_id) # Music Projects I've Heard
-    print(gnote.title) # validating correct note
+    print(f'Note Title: {gnote.title}') # validating correct note
     
     data = gnote.text     
     return data
 
+
+# update local text file with gkeep note data
+def update_local_txt(file_path, note_id=note_id):
     
+    gkeep_data = note_data()
+    
+    with open(file_path, 'w', encoding='utf-8') as textfile:
+        textfile.write(gkeep_data)
+    
+    print('Local Txt updated from Keep Note')
+
+
+# update gkeep note with local text file data
+def update_keep_note(file_path, note_id=note_id):
+    
+    keep = connect()
+    
+    # get latest local note data
+    with open(file_path, 'r', encoding='utf-8') as textfile:
+        local_data = textfile.read()
+    
+    # push data to google keep    
+    gnote = keep.get(note_id)
+    gnote.text = local_data
+    keep.sync()
+    
+    print('Keep Note updated from Local Txt')
+           
+   
 if __name__ == '__main__':
-       
-    with open('test.txt', 'w', encoding='utf-8') as f:
-        f.write(gkeep_get(email, master_token, note_id))
-        f.close()
     
+    mode = input('Test mode: ')
+    
+    if mode == 'data':
+        print(note_data(note_id))
+            
+    elif mode == 'ukeep':
+        update_keep_note('test.txt', note_id)
+        
+    elif mode == 'ulocal':
+        update_local_txt('test.txt', note_id)
